ef8786e7fe88e5d9356355240344294efdd4c0a4
= <=>
2020-05-12 20:41:12 +0530

    Weather API integrated for initial load

--- js/common/config.js
+++ js/common/config.js
@@ -1,6 +1,36 @@
 
 const config = {
     WEATHER_API_KEY: "b75dddf25be4051716ceb5c94cdf367f",
+    WEATHER_API_URL: "http://api.openweathermap.org/data/2.5/onecall?",
     GOOGLE_API_KEY: "AIzaSyCmdq1p2LZ_42tgPImsI3E6jSdz3S3lyP8",
     GOOGLE_API_URL: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input="
+}
+
+
+//API CALL
+apiRequest = (url, callback) => {
+    $.ajax({
+        url: url,
+        success: function (response) {
+            callback(response);
+        },
+        error: (err) => {
+            callback(err);
+        }
+    })
+
+}
+
+//HANDLEBARS BIND
+bindDataToDOM = (template, elem, data, isAList) => {
+    var templateElem = $('#' + template);
+    var bindingElem = $('#' + elem);
+    var temp = Handlebars.compile(templateElem[0].innerHTML);
+    var compiledData = temp(data);
+    if (isAList) {
+        bindingElem.append(compiledData);
+    }
+    else {
+        bindingElem.html(compiledData);
+    }
 }
--- js/scripts/chart.js
+++ js/scripts/chart.js
@@ -1,86 +1,90 @@
-var ctx = document.getElementById("myChart").getContext('2d');
 
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["9am", "10am", "11am", "12pm", "1pm", "2pm"],
-        datasets: [{
-            data: [24, 27, 28, 30, 45, 30],
-            fill: false,
-            borderColor: '#2196f3',
-            backgroundColor: '#2196f3',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        layout: {
-            padding: {
-                top: 15,
-                bottom: 15
-            }
-        },
-        legend: {
-            display: false
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    display: false
-                },
-                gridLines: {
-                    display: false,
-                }
-            }],
+initalizeCharts = (forecastData, ssData) => {
 
-            xAxes: [{
-                ticks: {
-                    padding: 10
-                },
-                gridLines: {
-                    drawBorder: false,
-                }
+    var ctx = document.getElementById("myChart").getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["9am", "10am", "11am", "12pm", "1pm", "2pm"],
+            datasets: [{
+                data: [24, 27, 28, 30, 45, 30],
+                fill: false,
+                borderColor: '#2196f3',
+                backgroundColor: '#2196f3',
+                borderWidth: 1
             }]
         },
-        responsive: true,
-        maintainAspectRatio: true,
-    }
-});
-
-var ctx2 = document.getElementById("ssChart").getContext('2d');
-
-var myChart2 = new Chart(ctx2, {
-    type: 'line',
-    data: {
-        labels: ["6am", "1pm", "8pm"],
-        datasets: [{
-            data: [-25, 50, -25],
-            fill: true,
-            borderColor: '#2196f3',
-            backgroundColor: '#2196f3',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        layout: {
-            padding: {
-                top: 15,
-                bottom: 15
-            }
-        },
-        legend: {
-            display: false
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    display: false
-                },
-                gridLines: {
-                    display: false,
+        options: {
+            layout: {
+                padding: {
+                    top: 15,
+                    bottom: 15
                 }
-            }],
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        display: false
+                    },
+                    gridLines: {
+                        display: false,
+                    }
+                }],
+
+                xAxes: [{
+                    ticks: {
+                        padding: 10
+                    },
+                    gridLines: {
+                        drawBorder: false,
+                    }
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: true,
+        }
+    });
+
+    var ctx2 = document.getElementById("ssChart").getContext('2d');
+
+    var myChart2 = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: ["6am", "1pm", "8pm"],
+            datasets: [{
+                data: [-25, 50, -25],
+                fill: true,
+                borderColor: '#2196f3',
+                backgroundColor: '#2196f3',
+                borderWidth: 1
+            }]
         },
-        responsive: true,
-        maintainAspectRatio: true,
-    }
-});
+        options: {
+            layout: {
+                padding: {
+                    top: 15,
+                    bottom: 15
+                }
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        display: false
+                    },
+                    gridLines: {
+                        display: false,
+                    }
+                }],
+            },
+            responsive: true,
+            maintainAspectRatio: true,
+        }
+    });
+}
